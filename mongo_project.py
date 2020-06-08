9855cc2a0dffe0282369e87544ad7324b37eeb47
Samantha Simmons <sammysimmons274@gmail.com>
2020-06-08 18:39:35 +0100

    inital commit

---
+++ mongo_project.py
@@ -0,0 +1,13 @@
+import pymongo
+import os
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "mytestdb"
+COLLECTION_NAME = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
